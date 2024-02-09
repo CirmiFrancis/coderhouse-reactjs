5459bade2d6add346e83242baedaca936e8e8b79
Francis Cirmi <cirmifrancis@gmail.com>
2024-02-09 18:00:58 -0300

    Carpeta ProyectoFinal agregado

--- mainReact/src/PreEntrega1/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import logoBK from "../assets/bk-logo.png"
-
-const Logo = ({tamanio}) => {
-    return (
-        <img src={logoBK} alt="Logo de Burger King" className="logo" width={tamanio} />
-    )
-}
-
-export default Logo;
