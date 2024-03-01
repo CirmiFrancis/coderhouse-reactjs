53aff6433c6700820e3404a4deecb40b24594c65
Francis Cirmi <cirmifrancis@gmail.com>
2024-03-01 03:28:53 -0300

    Game Detail Responsive entre 768px y 1440px

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -13,21 +13,21 @@ const ItemDetail = ({item}) => {
         <div className="container-fluid bg-gradient py-5 padding-custom">
             <div className="row">
                 <div className="col-md-5 text-center">
-                    <img src={item.image} alt={item.title} className="border border-light rounded" />
+                    <img src={item.image} alt={item.title} className="img-size border border-light rounded" />
                 </div>
 
                 <div className="col-md-7 d-flex flex-column justify-content-center">
                     <div className="row text-light font-gigames">
-                        <h2 className="m-0 fs-1">{item.title}</h2>
+                        <h2 className="title-size m-0">{item.title}</h2>
                     </div>
                     <div className="row text-light font-platform pt-2">
-                        <h2 className="m-0 fs-6">{item.platform.toUpperCase()}</h2>
+                        <h2 className="platform-size m-0">{item.platform.toUpperCase()}</h2>
                     </div>
                     <div className="row text-light font-platform fs-3 pt-3">
-                        <p className="m-0"><b>$ {item.price}</b></p>
+                        <p className="price-size m-0"><b>$ {item.price}</b></p>
                     </div>
                     <div className="row text-light font-text text-justify pt-5 pb-2">
-                        <p className="m-0">{item.description}</p>
+                        <p className="description-size m-0">{item.description}</p>
                     </div>
                     <div className="row">
                         <ItemCount stock={item.stock} onAdd={onAdd}/>
